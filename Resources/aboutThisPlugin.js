26b3261840993d53788ee3cb7e5869e127bcd0d5
MURAOKA Yusuke <yusuke@jbking.org>
2018-07-28 15:34:31 +0900

    formatted

--- Resources/aboutThisPlugin.js
+++ Resources/aboutThisPlugin.js
@@ -1,39 +1,40 @@
-var _ = function(){
-	var action = new PlugIn.Action(function(selection){
-		versNum = this.plugIn.version.versionString
-		pluginName = this.plugIn.displayName
-		pluginID = this.plugIn.identifier
-		pluginLibraries = this.plugIn.libraries
-		if (pluginLibraries.length != 0){
-			libraryNames = []
-			pluginLibraries.forEach(function(aLibrary){
-				libraryName = aLibrary.name
-				libraryVersion = aLibrary.version.versionString
-				displayString = libraryName + ' v' + libraryVersion
-				libraryNames.push(displayString)
-			})
-			libraryNamesString = "LIBRARIES:"
-			libraryNamesString = libraryNamesString + '\n' + libraryNames.join('\n')
-		} else {
-			libraryNamesString = "This plugin has no libraries."
-		}
-		alertTitle = pluginName + ' v.' + versNum
-		descriptionString = "A plugin demonstrating the use of Omni Automation to automate OmniGraffle in macOS and iOS."
-		companyURL = 'https://yourwebsiteurl.com'
-		alertMessage = "©2017 The PlugIn Author" + '\n'
-		alertMessage = alertMessage + pluginID + '\n'
-		alertMessage = alertMessage + companyURL + '\n' + '\n'
-		alertMessage = alertMessage + descriptionString + '\n' + '\n' 
-		alertMessage = alertMessage + libraryNamesString
-		var alert = new Alert(alertTitle, alertMessage)
-		alert.show(function(result){})
-	});
+var _ = (function() {
+  var action = new PlugIn.Action(function(selection) {
+    versNum = this.plugIn.version.versionString;
+    pluginName = this.plugIn.displayName;
+    pluginID = this.plugIn.identifier;
+    pluginLibraries = this.plugIn.libraries;
+    if (pluginLibraries.length != 0) {
+      libraryNames = [];
+      pluginLibraries.forEach(function(aLibrary) {
+        libraryName = aLibrary.name;
+        libraryVersion = aLibrary.version.versionString;
+        displayString = libraryName + " v" + libraryVersion;
+        libraryNames.push(displayString);
+      });
+      libraryNamesString = "LIBRARIES:";
+      libraryNamesString = libraryNamesString + "\n" + libraryNames.join("\n");
+    } else {
+      libraryNamesString = "This plugin has no libraries.";
+    }
+    alertTitle = pluginName + " v." + versNum;
+    descriptionString =
+      "A plugin demonstrating the use of Omni Automation to automate OmniGraffle in macOS and iOS.";
+    companyURL = "https://yourwebsiteurl.com";
+    alertMessage = "©2017 The PlugIn Author" + "\n";
+    alertMessage = alertMessage + pluginID + "\n";
+    alertMessage = alertMessage + companyURL + "\n" + "\n";
+    alertMessage = alertMessage + descriptionString + "\n" + "\n";
+    alertMessage = alertMessage + libraryNamesString;
+    var alert = new Alert(alertTitle, alertMessage);
+    alert.show(function(result) {});
+  });
 
-	// routine determines if menu item is enabled
-	action.validate = function(selection){
-		return true
-	};
+  // routine determines if menu item is enabled
+  action.validate = function(selection) {
+    return true;
+  };
 
-	return action;
-}();
+  return action;
+})();
 _;
--- Resources/mergeNodes.js
+++ Resources/mergeNodes.js
@@ -1,34 +1,51 @@
-var _ = function () {
-	var action = new PlugIn.Action(function (selection) {
-		// if called externally (from script) then generate selection array
-		if (typeof selection == 'undefined') {
-			// convert nodes into items
-			selectedItems = document.editors[0].selectedNodes.map(function (node) { return node.object });
-		} else {
-			selectedItems = selection.items;
-		}
-		// this array is shuffled often. so, sort it.
-		selectedItems.sort(function (a, b) { return a.index - b.index; });
-		firstItem = selectedItems[0];
-		var topic = selectedItems.map(function (item) { return item.topic }).join("\n");
-		firstItem.parent.addChild(firstItem.before, function (item) { item.topic = topic; });
-		selectedItems.forEach(function (item) {
-			item.remove();
-		});
-	});
+var _ = (function() {
+  var action = new PlugIn.Action(function(selection) {
+    // if called externally (from script) then generate selection array
+    if (typeof selection == "undefined") {
+      // convert nodes into items
+      selectedItems = document.editors[0].selectedNodes.map(function(node) {
+        return node.object;
+      });
+    } else {
+      selectedItems = selection.items;
+    }
+    // this array is shuffled often. so, sort it.
+    selectedItems.sort(function(a, b) {
+      return a.index - b.index;
+    });
+    firstItem = selectedItems[0];
+    var topic = selectedItems
+      .map(function(item) {
+        return item.topic;
+      })
+      .join("\n");
+    firstItem.parent.addChild(firstItem.before, function(item) {
+      item.topic = topic;
+    });
+    selectedItems.forEach(function(item) {
+      item.remove();
+    });
+  });
 
-	action.validate = function (selection) {
-		var selectedItems;
-		if (typeof selection == 'undefined') {
-			selectedItems = document.editors[0].selectedNodes.map(function (node) { return node.object });
-		} else {
-			selectedItems = selection.items;
-		}
-		var firstItemParent = selectedItems[0].parent;
-		// to avoid accident, valid only if selected some nodes and they are children of same parent.
-		return selectedItems.length > 1 && selectedItems.every(function (item) { return item.parent == firstItemParent; });
-	};
+  action.validate = function(selection) {
+    var selectedItems;
+    if (typeof selection == "undefined") {
+      selectedItems = document.editors[0].selectedNodes.map(function(node) {
+        return node.object;
+      });
+    } else {
+      selectedItems = selection.items;
+    }
+    var firstItemParent = selectedItems[0].parent;
+    // to avoid accident, valid only if selected some nodes and they are children of same parent.
+    return (
+      selectedItems.length > 1 &&
+      selectedItems.every(function(item) {
+        return item.parent == firstItemParent;
+      })
+    );
+  };
 
-	return action;
-}();
+  return action;
+})();
 _;
